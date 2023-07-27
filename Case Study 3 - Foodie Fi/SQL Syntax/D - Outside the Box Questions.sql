a571f658f44be694c2fbe2c0a9da975e66d9615f
Jay Kim <56371474+JayKim-Analytics@users.noreply.github.com>
2023-07-27 13:03:31 -0700

    Create D - Outside the Box Questions.sql

---
+++ Case Study 3 - Foodie Fi/SQL Syntax/D - Outside the Box Questions.sql
@@ -0,0 +1,62 @@
+/* --------------------------------
+   -- CASE STUDY #3: FOODIE - FI --
+   --------------------------------- */
+
+--Author: Jay Kim
+--Completed using: Microsoft SQL Server Management Studio 18
+
+
+/* -------------------------------
+-- D. Outside The Box Questions --
+------------------------------- */
+
+/* 1. How would you calculate the rate of growth for Foodie-Fi?
+	1. # of subscriptions over time
+	2. # of basic plans upgraded to pro plans over time
+	3. # of monthly plans upgrade to annual plans
+	4. # of monthly plans that stay subscribed for over 3/6/12 months
+*/
+
+/* 2. What key metrics would you recommend Foodie-Fi management to track over time to assess performance of their overall business?
+	1. # of basic plans upgraded to pro plans over time
+	2. # of monthly plans upgrade to annual plans
+	3. # of monthly plans that stay subscribed for over 3/6/12+ months
+	4. # of shows watched during trial period
+	5. # of basic plan users that reach their watch time limit
+	6. # of pro plan users that down-grade to a basic plan
+	7. # of basic plan subscribers vs pro plan vs total subscriber count
+*/
+
+/* 3. What are some key customer journeys or experiences that you would analyse further to improve customer retention?
+	1. Time spent watching shows on Foodie-Fi
+	2. # of unique shows watched 
+	3. Average time spent on Foodie-Fi 
+	4. Average time spent watching Foodie-Fi after initial subscription
+	5. Average time spent watching Foodi-Fi prior to churn
+	6. # of basic plan users that reach their watch time limit
+
+*/
+
+/* 4. If the Foodie-Fi team were to create an exit survey shown to customers who wish to cancel their subscription, what questions would you include in the survey?
+	1. Demographic information (Age, gender, identity)
+	2. Hours spent watching Foodie-Fi on a weekly basis 
+	3. Why are you cancelling your subscription?
+		- Does not fit in budget, content does not appeal to me, lack of variety in content, subscription is too expensive, other
+	4. Questions related to cooking content
+		- How interested are you in cooking as a skill?
+		- How interested are you in observing cooking competitions?
+		- Learning about restuarants (Guys Big Bite/Diners, Drive-Ins and Dives, etc)
+		- Cooking-adjacent drama/reality (hell's Kitchen, Martha & Snoop's, etc)
+	5. Questions about Foodie-Fi functionality
+		- Website/App design, streaming speed, video quality, user interface, etc
+	6. What did you enjoy about Foodie-Fi? (long-form)
+	7. What could be improved about Foodie-Fi? (long-form)
+	8. How likely are you to recommend Foodie-Fi to a friend?
+*/
+
+/* 5. What business levers could the Foodie-Fi team use to reduce the customer churn rate? How would you validate the effectiveness of your ideas?
+	1. Duo function -> two accounts under 1 subscription 
+		- Measure subscription length/churn for Duo accounts
+	2. Promotional Deals
+		- IE: refer a friend and get first month of access free
+*/
